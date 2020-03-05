6ef0093d57920f0a300976ade697d227a639f48e
rksmile <40001585+rksmile@users.noreply.github.com>
2020-03-05 22:54:50 +0100

    Update individual_generation.py

--- individual_generation.py
+++ individual_generation.py
@@ -1,6 +1,10 @@
 import pandas as pd 
 import numpy as np 
 import random 
+import sys
+
+filename = sys.argv[1]
+size = int(sys.argv[2])
 
 individual_dict = {"id": [], "name": [], "born_immunity_integrity": [], "attained_immunity_debuff": [], "healthy_eater": [], "smoker": [], "drinker": [], "infected": [], "quarantined": [], "dead": []}
 
@@ -13,7 +17,7 @@ def id_generation():
 
 
 def name_generation():
-	name = "%s. %s" % (alphalist[random.randint(25)], alphalist[random.randint(0, 25)])
+	name = "%s. %s" % (alphalist[random.randint(0, 25)], alphalist[random.randint(0, 25)])
 	return name
 
 
@@ -49,7 +53,7 @@ def smoking_habit_generator():
 
 
 def drinking_habit_generator():
-	randbool = random.randit(1, 100)
+	randbool = random.randint(1, 100)
 	drinker = False
 	if randbool < 56: # based on the NSDUH 2015 stats, im not very rigorous rn 
 		drinker = True
@@ -59,3 +63,35 @@ def drinking_habit_generator():
 
 # other parameters are determined during the "simulation"
 
+for x in range(size):
+	individual_dict["name"].append(name_generation())
+	individual_dict["id"].append(id_generation())
+	individual_dict["born_immunity_integrity"].append(immune_def_born())
+	individual_dict["attained_immunity_debuff"].append(0)
+	individual_dict["healthy_eater"].append(eating_habits_generator())
+	individual_dict["smoker"].append(smoking_habit_generator())
+	individual_dict["drinker"].append(drinking_habit_generator())
+	individual_dict["infected"].append(0)
+	individual_dict["quarantined"].append(0)
+	individual_dict["dead"].append(0)
+
+ind = individual_dict["born_immunity_integrity"].index(min(individual_dict["born_immunity_integrity"]))
+individual_dict["infected"][ind] = 1
+
+
+individual_df = pd.DataFrame(individual_dict, columns=["id", "name", "born_immunity_integrity", "attained_immunity_debuff", "healthy_eater", "smoker", "drinker", "infected", "quarantined", "dead"])
+
+# debug wall 
+# print(individual_dict["id"][:10])
+# print(individual_dict["name"][:10])
+# print(individual_dict["born_immunity_integrity"][:10])
+# print(individual_dict["attained_immunity_debuff"][:10])
+# print(individual_dict["healthy_eater"][:10])
+# print(individual_dict["smoker"][:10])
+# print(individual_dict["drinker"][:10])
+# print(individual_dict["infected"][:10])
+# print(individual_dict["quarantined"][:10])
+# print(individual_dict["dead"][:10])
+# print(individual_df.head(10))
+
+individual_df.to_csv("%s.csv" % filename)
